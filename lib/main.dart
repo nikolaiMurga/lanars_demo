e0b950abef85c56784748bbafc3f0bc0a1468e39
nikolaiMurga <nicolai.murga@gmail.com>
2024-07-21 16:50:19 +0200

    - fixed imports

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,16 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:lanars_demo/data/repo_impl/repo_impl.dart';
-import 'package:lanars_demo/domain/mappers/user_mapper.dart';
-import 'package:lanars_demo/domain/repo/repo.dart';
 
 import 'data/network/api_client.dart';
 import 'data/network/api_client_impl.dart';
 import 'data/repo_impl/auth_repo_impl.dart';
 import 'data/repo_impl/network_repo.dart';
+import 'data/repo_impl/repo_impl.dart';
 import 'data/repo_impl/secure_storage.dart';
+import 'domain/mappers/user_mapper.dart';
 import 'domain/repo/auth_repo.dart';
+import 'domain/repo/repo.dart';
 import 'lanars_demo_app.dart';
 
 void main() async {
